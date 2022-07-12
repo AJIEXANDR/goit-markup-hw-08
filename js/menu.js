0d529d27d34aec25d7f90c66bc2d7cfcb3217ad9
Aleksandr Tkachenko <saylass@ukr.net>
2022-07-12 23:08:28 +0300

    finished

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,16 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.page-header__open-btn'),
-    closeMenuBtn: document.querySelector('.page-header__close-btn'),
-    menu: document.querySelector('.menu-container'),
-    body: document.querySelector('body'),
+    openMenuBtn: document.querySelector(".page-header__open-btn"),
+    closeMenuBtn: document.querySelector(".page-header__close-btn"),
+    menu: document.querySelector(".menu-container"),
+    body: document.querySelector("body"),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
   function toggleMenu() {
-    refs.menu.classList.toggle('is-open');
-    refs.body.classList.toggle('no-scroll');
+    refs.menu.classList.toggle("is-closed");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
